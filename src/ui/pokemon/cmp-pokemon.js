83cd754617faee9e058df262fc641dbf9cf61d7a
jhflorey <jhflorey@gmail.com>
2020-01-21 08:25:00 +0700

    add react pokedex

---
+++ src/ui/pokemon/cmp-pokemon.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react'
+import ga from '../../utils/ga'
+
+class Pokemon extends PureComponent {
+  render() {
+    const { pokemon } = this.props
+
+    return (
+      <div className="pokemon">
+        <button
+          type="button"
+          className="pokemon__sprite"
+          onClick={() => {
+            ga.event({
+              category: 'pokemon',
+              action: 'click',
+              label: pokemon.id
+            })
+          }}
+          style={{
+            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              pokemon.id
+            }.png`})`
+          }}
+        />
+        <p className="pokemon__name">{pokemon.name}</p>
+      </div>
+    )
+  }
+}
+
+export default Pokemon
